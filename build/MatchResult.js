085ab36988b1edbcaba800952b52905da8cc5934
Billi Ramirez <ramirezbilly42@outlook.com>
2019-11-15 11:44:21 -0600

    adding generic class

---
+++ build/MatchResult.js
@@ -0,0 +1,9 @@
+"use strict";
+// Enum - Enumeration => Close related Values - small fixed values
+Object.defineProperty(exports, "__esModule", { value: true });
+var MatchResult;
+(function (MatchResult) {
+    MatchResult["HomeWin"] = "H";
+    MatchResult["AwayWin"] = "A";
+    MatchResult["Draw"] = "D";
+})(MatchResult = exports.MatchResult || (exports.MatchResult = {}));
---
+++ src/MatchResult.ts
@@ -0,0 +1,7 @@
+// Enum - Enumeration => Close related Values - small fixed values
+
+export enum MatchResult {
+  HomeWin = "H",
+  AwayWin = "A",
+  Draw = "D"
+}
